3d5d5a8e1d0d379d2acba29555a910d8f39a06a4
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-11-20 10:25:50 +1030

    add if condition to change devoured to opposite true/false value, which fixed put statement!

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -22,18 +22,23 @@ router.post("/api/burgers", function(req, res){
 });
 
 router.put("/api/burgers/:id", function(req, res){
-    var condition = "id = " + req.params.id;
-
+    const condition = "id = " + req.params.id;
+    let devouredProp;
     console.log("condition", condition);
-  
+   if (req.body.devoured==1){
+        devouredProp = '0';
+   } else if (req.body.devoured ==0) {
+        devouredProp = '1';
+   }
     burger.updateOne(
       {
-        devoured: req.body.devoured
+        devoured: devouredProp
       },
       condition,
       function(result) {
         if (result.changedRows === 0) {
           // If no rows were changed, then the ID must not exist, so 404
+          console.log("no rows changed");
           return res.status(404).end();
         }
         res.status(200).end();
